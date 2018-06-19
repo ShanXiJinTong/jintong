8836a29992cce0f404ea743bec34a4c0e6bff2b1
mszsnn <413871357@qq.com>
2018-06-19 19:12:47 +0800

    'asdf'

---
+++ src/assets/js/rem.js
@@ -0,0 +1,20 @@
+(function (doc, win) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=750){
+                  // 这里的350 取决于设计稿的宽度
+                    docEl.style.fontSize = '100px';
+                }else{
+                    docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+                }
+            };
+
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        recalc()
+})(document, window);
+
+
