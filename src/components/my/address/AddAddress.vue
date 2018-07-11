bf6dd08950a4fc4a48d59662888840a783a8169a
liXingStar <1459294281@qq.com>
2018-07-11 19:50:53 +0800

    '修改my'

--- src/components/my/address/AddAddress.vue
+++ src/components/my/address/AddAddress.vue
@@ -1,5 +1,38 @@
 <template>
-
+  <div class="content">
+    <div class="mwq-box">
+      <div class="mwq-title">
+        <div class="mwq-juxing"></div>
+        <div class="mwq-wenzi">联系人</div>
+      </div>
+      <div class="mwq-textarea">姓名</div>
+    </div>
+    <div class="mwq-box">
+      <div class="mwq-title">
+        <div class="mwq-juxing"></div>
+        <div class="mwq-wenzi">联系方式</div>
+      </div>
+      <div class="mwq-textarea">电话</div>
+    </div>
+    <div class="mwq-box">
+      <div class="mwq-title">
+        <div class="mwq-juxing"></div>
+        <div class="mwq-wenzi">服务地址</div>
+        <div class="mwq-enter">
+          <img src="../static/img/enter.png" alt="">
+        </div>
+      </div>
+      <div class="mwq-textarea"></div>
+    </div>
+    <div class="mwq-box">
+      <div class="mwq-title">
+        <div class="mwq-juxing"></div>
+        <div class="mwq-wenzi">门牌号</div>
+      </div>
+      <div class="mwq-textarea"></div>
+    </div>
+    <div class="mwq-reserve">保存</div>
+  </div>
 </template>
 <script>
     export default {
@@ -10,4 +43,5 @@
     }
 </script>
 <style scoped>
+  @import url(../static/css/Tthree.css);
 </style>
--- src/components/my/help/Help.vue
+++ src/components/my/help/Help.vue
@@ -1,5 +1,26 @@
 <template>
-
+  <div class="shdBox">
+    <ul class="totalList">
+      <a href="shd-helpContent.html">
+        <li class="list">
+          <div class="listLeft">
+            <img src="../static/img/shd-img/tubiao.png" alt="">
+            <span>如何预约上门服务</span>
+          </div>
+          <img src="../static/img/shd-img/shdBack1.png" alt="">
+        </li>
+      </a>
+      <a href="">
+        <li class="list">
+          <div class="listLeft">
+            <img src="../static/img/shd-img/tubiao.png" alt="">
+            <span>如何下单</span>
+          </div>
+          <img src="../static/img/shd-img/shdBack1.png" alt="">
+        </li>
+      </a>
+    </ul>
+  </div>
 </template>
 <script>
     export default {
@@ -10,4 +31,5 @@
     }
 </script>
 <style scoped>
+  @import url(../static/css/shd-help.css);
 </style>
