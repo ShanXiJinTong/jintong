24f556ac126c83affa44cb16f1a52101a331532d
liXingStar <1459294281@qq.com>
2018-07-12 16:40:59 +0800

    模板搭建完成

--- src/components/my/vip/VipImprove.vue
+++ src/components/my/vip/VipImprove.vue
@@ -1,5 +1,43 @@
 <template>
-
+<div>
+  <div class="content">
+    <img src="../static/img/gb-pic/gb.jpg" alt="" width="100%" height="100%"><span>会员<p>等级介绍</p></span>
+  </div>
+  <div class="xian"></div>
+  <div class="middle">
+    <ul style="color: #31d094;">
+      <li ><p>累计充值300元可成为白银会员 即可享受白银会员特权</p></li>
+      <li ><p>累计充值1000元可成为黄金会员 即可享受黄金会员特权</p></li>
+      <li ><p>累计充值2000元可成为白金会员 即可享受白金会员特权</p></li>
+    </ul>
+  </div>
+  <!--第二部分-->
+  <div class="content">
+    <img src="../static/img/gb-pic/gb.jpg" alt="" width="100%" height="100%"><span>会员<b>特权介绍</b></span>
+  </div>
+  <div class="xian"></div>
+  <div class="down">
+    <img src="../static/img/gb-pic/changxian.png" alt="">
+    <div class="kuai">
+      <h5>新服务尝鲜</h5>
+      <h6>白银及以上会员可以在第一时间免费享受新服务特权</h6>
+    </div>
+  </div>
+  <div class="down">
+    <img src="../static/img/gb-pic/zhekou.png" alt="">
+    <div class="kuai">
+      <h5>会员折扣</h5>
+      <h6>黄金及以上会员可以在第一时间免费享受新服务特权</h6>
+    </div>
+  </div>
+  <div class="down">
+    <img src="../static/img/gb-pic/fuwu.png" alt="">
+    <div class="kuai">
+      <h5>会员快速服务</h5>
+      <h6>黄金及以上会员可以在第一时间免费享受新服务特权以及快速服务无需排队</h6>
+    </div>
+  </div>
+</div>
 </template>
 <script>
     export default {
@@ -10,4 +48,5 @@
     }
 </script>
 <style scoped>
+  @import url(../static/css/gb-JT2.css);
 </style>
