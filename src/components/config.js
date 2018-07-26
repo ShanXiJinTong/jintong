92792c376b0c1181e9546d0b16f5251717500567
liXingStar <1459294281@qq.com>
2018-07-26 14:20:09 +0800

    'order'

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 export default {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'access-token': 'sHe5WYSjPu0a7not04qB-F5Sgu_TYe8x',
+  'access-token': 'K3yofCYfSqrCmXmkHEsldNTgfIdxHr6I',
   'fecshop-uuid': '8f682f66-88eb-11e8-bed6-00163e021360'
 };
+// 'Content-Type': 'application/x-www-form-urlencoded',
