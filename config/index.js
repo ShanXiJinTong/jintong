45e4d75adf73bad0d7647b45d327275dae8af4af
pjb-pjb <2939355301@qq.com>
2018-08-15 13:45:30 +0800

    123

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '*': {
-        target: 'http://appserver.chengzhanghao.com:81/',
+        target: 'http://appserver.uekuek.com/',
         changeOrigin: true
       }
     },
--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -32,21 +32,6 @@
   </div>
 </template>
 <script>
-  //  import wx from "weixin-js-sdk"
-  //  wx.config({
-  //     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-  //     appId: 'wx6365026f119bdb02', // 必填，公众号的唯一标识
-  //     timestamp:1414587457, // 必填，生成签名的时间戳
-  //     nonceStr: 'Wm3WZYTPz0wzccnW', // 必填，生成签名的随机串
-  //     signature: '87b424819f1f4445d3ba81bd08a3a3013139e0ab',// 必填，签名
-  //     jsApiList: ['chooseImage']         // 必填，需要使用的JS接口列表
-  // });
-  //
-  //  wx.error(function(res){//通过error接口处理失败验证
-  //    console.log(res);
-  //    // config信息验证失败会执行error函数
-  //  });
-
 
   import Tab from "../common/tab"
   export default {
